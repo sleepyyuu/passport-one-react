5a24ea978bb73c71fd153f8416409052710844a1
Yu <k28143995@gmail.com>
2022-06-14 19:49:37 -0700

    Transition from reddit to pushshift api for larger dataset

--- src/assets/requestData.js
+++ src/assets/requestData.js
@@ -10,25 +10,38 @@ async function requestData() {
           mode: "cors",
         });
         response = await response.json();
-        response = response.data.children;
+        response = response.data;
         for (let submission of response) {
-          submission.data.postedDate = new Date(1000 * submission.data.created_utc);
-          allResponses.push(submission.data);
+          submission.postedDate = new Date(1000 * submission.created_utc);
+          allResponses.push(submission);
         }
+        // response = await response.json();
+        // response = response.data.children;
+        // for (let submission of response) {
+        //   submission.data.postedDate = new Date(1000 * submission.data.created_utc);
+        //   allResponses.push(submission.data);
+        // }
         //utiilize api "after" to get more than 100 results
-        lastRenewalObject = response[response.length - 1].data;
+        // lastRenewalObject = response[response.length - 1].data;
+        // let count = 100;
+        // let lastID = lastRenewalObject.id;
+        // dataUrl = url + "&count=" + count + "&after=t3_" + lastID;
+        // count += 100;
+        lastRenewalObject = response[response.length - 1];
         let count = 100;
-        let lastID = lastRenewalObject.id;
-        dataUrl = url + "&count=" + count + "&after=t3_" + lastID;
+        let lastTime = lastRenewalObject.created_utc;
+        dataUrl = url + "&before=" + lastTime;
         count += 100;
-      } while (response.length === 100);
+      } while (response.length > 20);
     } catch (error) {
       return allResponses;
     }
   };
 
+  //api endpoint https://old.reddit.com/r/Passports/search/.json?sort=new&restrict_sr=on&q=timeline&limit=100
+
   Promise.all([
-    await getSubmission("https://old.reddit.com/r/Passports/search/.json?sort=new&restrict_sr=on&q=timeline&limit=100"),
+    await getSubmission("https://api.pushshift.io/reddit/search/submission/?q=timeline&subreddit=passports&limit=100&sort=desc"),
   ]);
   return allResponses;
 }
